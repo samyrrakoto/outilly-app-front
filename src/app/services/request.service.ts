4e14799d5f804c31719badec16bef739bbe48c0d
Kyromane <c.gauthier94250@gmail.com>
2020-06-24 14:26:16 +0200

    advanced checks for signup form

--- src/app/services/request.service.ts
+++ src/app/services/request.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { Uri } from '../models/uri';
 import { HttpHeaders, HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
