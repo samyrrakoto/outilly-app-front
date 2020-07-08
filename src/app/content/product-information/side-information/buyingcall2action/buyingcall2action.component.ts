7c8c49425a5c23d06019de70c6fafcd4533ae37e
Kyromane <c.gauthier94250@gmail.com>
2020-07-09 00:21:51 +0200

    Completed maping of product information for side information

--- src/app/content/product-information/side-information/buyingcall2action/buyingcall2action.component.ts
+++ src/app/content/product-information/side-information/buyingcall2action/buyingcall2action.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductInformationComponent } from '../../product-information.component';
+import { RequestService } from 'src/app/services/request.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'buying-call-2-action',
   templateUrl: './buyingcall2action.component.html',
   styleUrls: ['./buyingcall2action.component.css']
 })
-export class Buyingcall2actionComponent implements OnInit {
+export class Buyingcall2actionComponent extends ProductInformationComponent implements OnInit {
 
-  constructor() { }
+  constructor(request: RequestService, route: ActivatedRoute) {
+    super(request, route);
+  }
 
   ngOnInit(): void {
   }
