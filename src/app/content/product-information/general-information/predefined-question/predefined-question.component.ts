5c5a615e5a2621dda535af9f5e2d6b3e9fcd2a3f
Kyromane <c.gauthier94250@gmail.com>
2020-07-24 16:31:17 +0200

    Removed questions array from PredefinedQuestionComponent since questions are got back from API

--- src/app/content/product-information/general-information/predefined-question/predefined-question.component.ts
+++ src/app/content/product-information/general-information/predefined-question/predefined-question.component.ts
@@ -9,17 +9,6 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./predefined-question.component.scss']
 })
 export class PredefinedQuestionComponent extends ProductInformationComponent implements OnInit {
-  questions: Array<string> = [
-    'Quel est le prix de cet objet ?',
-    'Le prix est-il négociable ?',
-    'Le lot est-il 100% fonctionnel ?',
-    'Où se trouve le produit pour le récupérer en mains propres ?'];
-  answers: Array<string> = [
-    'Je vends cet article 140€ TTC.',
-    'Peut-être.',
-    'Ca dépend.',
-    'Oui'
-  ];
 
   constructor(request: RequestService, route: ActivatedRoute) {
     super(request, route);
