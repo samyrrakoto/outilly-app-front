58ed6d217bcf2c9c76185e38a4a80e3edc902fca
Samyr Rakoto <samyr.rakoto@loutillerie.com>
2020-10-19 23:38:47 +0200

    Initialized mondial relay PR selector for good

--- src/app/content/product-information/checkout/delivery-information/delivery-information.component.ts
+++ src/app/content/product-information/checkout/delivery-information/delivery-information.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import * as $ from 'jquery';
 
 @Component({
   selector: 'app-delivery-information',
@@ -7,16 +6,15 @@ import * as $ from 'jquery';
   styleUrls: ['./delivery-information.component.css']
 })
 export class DeliveryInformationComponent implements OnInit {
-
+  $: any;
   constructor() { }
 
-  ngOnInit(): void {}
-
-  ngAfterViewChecked(): void {
+  ngOnInit(): void {
     this.mondialRelayWidget();
   }
 
   public mondialRelayWidget(): void {
+    // @ts-ignore
     $("#zone_widget").MR_ParcelShopPicker({
         Target: "#data",
         Brand: "BDTEST13",
