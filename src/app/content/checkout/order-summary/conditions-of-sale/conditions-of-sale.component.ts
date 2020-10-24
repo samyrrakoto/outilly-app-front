255d818172f0b120f954d5f025e0ce4a9ace33de
Kyromane <c.gauthier94250@gmail.com>
2020-10-25 01:35:43 +0200

    Style changes so that the page fits the mockup

--- src/app/content/checkout/order-summary/conditions-of-sale/conditions-of-sale.component.ts
+++ src/app/content/checkout/order-summary/conditions-of-sale/conditions-of-sale.component.ts
@@ -1,13 +1,19 @@
+import { Modals } from './../../../../models/modals';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-conditions-of-sale',
   templateUrl: './conditions-of-sale.component.html',
-  styleUrls: ['./conditions-of-sale.component.css']
+  styleUrls: ['../order-summary.component.css', './conditions-of-sale.component.css']
 })
 export class ConditionsOfSaleComponent implements OnInit {
+  modals: Modals;
 
-  constructor() { }
+  constructor() {
+    this.modals = new Modals();
+    this.modals.addModal('conditions-of-use');
+    this.modals.addModal('conditions-of-sale');
+  }
 
   ngOnInit(): void {
   }
