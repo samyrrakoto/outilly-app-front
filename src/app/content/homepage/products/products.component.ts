db0c53b92705ef6d6011b9b02a211d04f84239b8
Klaymant <c.gauthier94250@gmail.com>
2020-11-24 20:18:35 +0100

    First static element of products

--- src/app/content/homepage/products/products.component.ts
+++ src/app/content/homepage/products/products.component.ts
@@ -6,8 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
+  testImg: any[];
 
-  constructor() { }
+  constructor() {
+    this.testImg = [
+      {'path': 'assets/img/product/tool1.jpg', 'productName': 'Outil 1'},
+      {'path': 'assets/img/product/tool2.jpg', 'productName': 'Outil 2'},
+      {'path': 'assets/img/product/tool3.jpg', 'productName': 'Outil 3'},
+      {'path': 'assets/img/product/tool4.jpg', 'productName': 'Outil 4'},
+      {'path': 'assets/img/product/tool5.jpg', 'productName': 'Outil 5'}
+    ];
+  }
 
   ngOnInit(): void {
   }
