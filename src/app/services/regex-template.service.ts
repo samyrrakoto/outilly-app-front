17358b2259a2c1b37f6949743ef4fb7ba136e284
Klaymant <c.gauthier94250@gmail.com>
2020-12-05 21:13:10 +0100

    Accepts phone number starting with 09

--- src/app/services/regex-template.service.ts
+++ src/app/services/regex-template.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class RegexTemplateService {
-  readonly PHONE: RegExp = /^0{1}[1234567]{1}[0-9]{8}$/;
+  readonly PHONE: RegExp = /^0{1}[12345679]{1}[0-9]{8}$/;
   readonly LINE1: RegExp = /^$/;
   readonly ZIPCODE: RegExp = /^[0-9]{4,5}$/;
 }
