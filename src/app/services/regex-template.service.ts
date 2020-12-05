a78225fe14f444f545e6b3d19f4d220cf9763c7e
Klaymant <c.gauthier94250@gmail.com>
2020-12-05 21:36:56 +0100

    Accepts phone numbers starting with +33, 0033 or 00

--- src/app/services/regex-template.service.ts
+++ src/app/services/regex-template.service.ts
@@ -4,7 +4,6 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class RegexTemplateService {
-  readonly PHONE: RegExp = /^0{1}[123456789]{1}[0-9]{8}$/;
-  readonly LINE1: RegExp = /^$/;
+  readonly PHONE: RegExp = /^((00|\+)33|0)[0-9][0-9]{8}$/;
   readonly ZIPCODE: RegExp = /^[0-9]{4,5}$/;
 }
