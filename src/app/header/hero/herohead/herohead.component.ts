f4b45ff686f4dceffe86d35eecf4b419cfbb4a53
Klaymant <c.gauthier94250@gmail.com>
2020-12-08 01:07:45 +0100

    Hides header and menu on product and account onboardings

--- src/app/header/hero/herohead/herohead.component.ts
+++ src/app/header/hero/herohead/herohead.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-herohead',
@@ -11,5 +11,4 @@ export class HeroheadComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
 }
--- src/app/header/hero/herohead/menu/menu.component.ts
+++ src/app/header/hero/herohead/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
@@ -6,9 +6,9 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
-
   @ViewChild('navBurger') navBurger: ElementRef;
   @ViewChild('navBarMenu') navBarMenu: ElementRef;
+
   constructor() { }
 
   ngOnInit(): void {
@@ -18,5 +18,4 @@ export class MenuComponent implements OnInit {
     this.navBurger.nativeElement.classList.toggle('is-active');
     this.navBarMenu.nativeElement.classList.toggle('is-active');
   }
-
 }
