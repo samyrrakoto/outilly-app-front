dc94fe9c695f711b89fe14d116ed41e5bc6c38a4
Klaymant <c.gauthier94250@gmail.com>
2020-12-11 12:41:21 +0100

    Refacto of onboarding system part 1

---
+++ src/app/onboardings.ts
@@ -0,0 +1,21 @@
+export const accountOnboarding = [
+  'email',
+  'firstname',
+  'lastname',
+  'gender',
+  'status',
+  'status/company-name',
+  'status/siret',
+  'status/tva',
+  'birthdate',
+  'country',
+  'zipcode',
+  'city',
+  'street',
+  'phonenumber',
+  'password',
+  'passwordonfirmation',
+  'emailoptin',
+  'validation',
+  'confirmation'
+]
