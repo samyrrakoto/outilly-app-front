53f328b6352330fc046b68312b2132febd23de65
Samyr Rakoto <samyr.rakoto@loutillerie.com>
2020-12-16 01:45:58 +0100

    Revert "Updated prod env config and added preprod env"
    This reverts commit 3f54e85f5f494ae181cd85463d2c3266ab3537f4.

--- src/environments/environment.preprod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  apiBaseUri: 'https://api-preprod.outilly.com/',
-  mediaBaseUri: 'https://media-preprod.outilly.com/',
-  mondialBrand: 'CC21IBBI'
-};
