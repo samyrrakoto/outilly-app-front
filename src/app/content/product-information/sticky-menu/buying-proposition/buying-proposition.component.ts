bc84883aaa23ae19ce827273f0b1a61084f94807
Klaymant <c.gauthier94250@gmail.com>
2020-12-17 19:45:41 +0100

    Changed UI for offer confirmation in product page

--- src/app/content/product-information/sticky-menu/buying-proposition/buying-proposition.component.ts
+++ src/app/content/product-information/sticky-menu/buying-proposition/buying-proposition.component.ts
@@ -22,6 +22,7 @@ export class BuyingPropositionComponent implements OnInit {
   purchases: Array<Purchase>;
   currentPurchase: Purchase;
   id: number;
+  bidAmount: number = 0;
   bidPlaced: boolean = false;
 
   constructor(public request: RequestService,
@@ -41,13 +42,10 @@ export class BuyingPropositionComponent implements OnInit {
   ngOnInit(): void {}
 
   public placeBid(amount: number): void {
-    const message: string = 'Votre offre de ' + amount + '€ a bien été envoyée';
-    const classes: string[] = ['confirmation-notification'];
-
     if (this.checkBidValue(amount)) {
       this.bidManager.place(amount * 100, this.sale.id);
+      this.bidAmount = amount;
       this.bidPlaced = true;
-      this.notification.display(message, 'proposition', classes, -1);
     }
   }
 
