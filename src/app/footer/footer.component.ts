f94d1b2e5ec08c33d7694de7d3c7e7627c904250
Klaymant <c.gauthier94250@gmail.com>
2020-12-19 13:32:22 +0100

    Footer created

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,6 +6,18 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+  readonly footerBrands: string[] = [
+    'Facom',
+    'Bosch',
+    'Milwaukee',
+    'Makita',
+    'Tubesca',
+    'Hikoki',
+    'Dexter',
+    'Magnusson',
+    'DeWalt',
+    'Gardena'
+  ];
   @Input() toDisplay: boolean;
 
   constructor() { }
