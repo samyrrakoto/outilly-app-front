a9e9f4fdbd9775a8d5b4153b42e8254e1ddd3f2e
Klaymant <c.gauthier94250@gmail.com>
2020-12-22 11:24:15 +0100

    Limit product display by category to 1 in homepage on mobile devices

---
+++ src/app/models/array-toolbox.spec.ts
@@ -0,0 +1,7 @@
+import { ArrayToolbox } from './array-toolbox';
+
+describe('ArrayToolbox', () => {
+  it('should create an instance', () => {
+    expect(new ArrayToolbox()).toBeTruthy();
+  });
+});
---
+++ src/app/models/array-toolbox.ts
@@ -0,0 +1,14 @@
+export class ArrayToolbox {
+
+  constructor() {}
+
+  public shuffle(array: any[]): any[] {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * i);
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    return array;
+  }
+}
