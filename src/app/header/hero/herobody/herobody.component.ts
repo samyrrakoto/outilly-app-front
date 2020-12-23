e58e52de56c6f9bf43ceec8ae45767da1d3fa4b1
Klaymant <c.gauthier94250@gmail.com>
2020-12-23 23:19:31 +0100

    Added icons to brand promise

--- src/app/header/hero/herobody/herobody.component.ts
+++ src/app/header/hero/herobody/herobody.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { pageInfo } from 'src/app/parameters';
 import { Component, Input, OnInit } from '@angular/core';
 
@@ -10,9 +11,19 @@ export class HerobodyComponent implements OnInit {
   readonly brandName: string = pageInfo.BRAND_NAME;
   @Input() logged: boolean;
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
+  public launchCategorySearch(categoryId: number): void {
+    this.router.navigate(['/product-results'],
+    {
+      queryParams: {
+        category: categoryId.toString()
+      }
+    });
+  }
 }
