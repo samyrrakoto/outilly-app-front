ec8cb32604aeaae7b47538eda8424e736a21fbd5
Klaymant <c.gauthier94250@gmail.com>
2020-12-27 22:45:32 +0100

    Fixed bug : when creating a second time an account the data are already set

--- src/app/content/onboarding/confirmation/confirmation.component.ts
+++ src/app/content/onboarding/confirmation/confirmation.component.ts
@@ -2,6 +2,7 @@ import { pageInfo } from 'src/app/parameters';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormDataService } from 'src/app/services/form-data.service';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-confirmation',
@@ -11,8 +12,13 @@ import { FormDataService } from 'src/app/services/form-data.service';
 export class ConfirmationComponent implements OnInit {
   readonly brandName: string = pageInfo.BRAND_NAME;
 
-  constructor(private formData: FormDataService, public router: Router) {
+  constructor(
+    private formData: FormDataService,
+    public router: Router)
+  {
     localStorage.removeItem('formData');
+    this.formData.user = new User();
+    this.formData.isAccountComplete = false;
   }
 
   ngOnInit(): void {
