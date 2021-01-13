c6cd7dcbc6031d28c51eb9a0d87e66ba0bab2384
Klaymant <c.gauthier94250@gmail.com>
2021-01-13 19:48:22 +0100

    Changed sale availability route to avoid name conflict

--- src/app/models/uri.ts
+++ src/app/models/uri.ts
@@ -22,7 +22,7 @@ export class Uri {
   readonly GET_SALES_NEW: string = 'secure/sales/new';
   readonly GET_SALE: string = 'sale';
   readonly GET_SALE_VENDOR: string = 'secure/sale';
-  readonly GET_SALE_AVAILABILITY: string = 'sale/availability';
+  readonly GET_SALE_AVAILABILITY: string = 'sale/check/availability';
   readonly GET_BIDS_AND_SALES = 'secure/bids/sales';
   readonly BRANDS: string = 'brands';
   readonly CATEGORIES: string = 'product/categories/all';
