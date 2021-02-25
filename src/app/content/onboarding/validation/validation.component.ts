2867b419b5f040d4e82a951b0589a73656435561
Klaymant <c.gauthier94250@gmail.com>
2021-02-25 18:19:55 +0100

    Added CGVU in onboarding recap

--- src/app/content/onboarding/validation/validation.component.ts
+++ src/app/content/onboarding/validation/validation.component.ts
@@ -6,6 +6,7 @@ import { User } from 'src/app/models/user';
 import { FormDataService } from 'src/app/services/form-data.service';
 import { GenericComponent } from 'src/app/models/generic-component';
 import { Router } from '@angular/router';
+import { Modals } from 'src/app/models/modals';
 
 @Component({
   selector: 'app-validation',
@@ -14,6 +15,8 @@ import { Router } from '@angular/router';
 })
 export class ValidationComponent extends GenericComponent implements OnInit {
   user: User = new User();
+  conditionsAccepted: boolean = false;
+  modals: Modals = new Modals();
   public loading: boolean = false;
 
   constructor(
@@ -26,6 +29,7 @@ export class ValidationComponent extends GenericComponent implements OnInit {
     super();
     !this.formDataService.user.username ? this.formDataService.user = JSON.parse(localStorage.getItem('formData')).user : null;
     this.user = this.formDataService.user;
+    this.modals.addModal('conditions-of-use');
   }
 
   ngOnInit() {
@@ -38,5 +42,9 @@ export class ValidationComponent extends GenericComponent implements OnInit {
         this.router.navigate(['onboarding/confirmation'])
       })
       .catch();
+  }
+
+  public changeConditionsStatus(): void {
+    this.conditionsAccepted = !this.conditionsAccepted;
   }
 }
