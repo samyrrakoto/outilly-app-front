ca7e260393758419074b0889ae658ae57a3c2219
Klaymant <c.gauthier94250@gmail.com>
2021-03-19 00:20:44 +0100

    Added categories, types, state & delivery filters for desktop

--- src/app/models/search-query.ts
+++ src/app/models/search-query.ts
@@ -3,6 +3,7 @@ import { GeoSearch, GeoService } from "src/app/services/geo.service";
 
 export class SearchQuery {
   searchParams: SearchParams;
+  filters: Filters = new Filters();
   geo: GeoSearch = new GeoSearch('0', '0', 0);
   zipcode: string = '';
   slider: number = 0;
@@ -60,4 +61,82 @@ type SearchParams = {
   page?: number;
   aroundLatLng?: string;
   aroundRadius?: number;
+}
+
+class Filters {
+  brands: string[] = [];
+  quality: string[] = [];
+  types: string[] = [];
+  categories: string[] = [];
+  to_deliver: string[] = [];
+
+  public getAll(): string {
+    let request = this.constructFilters();
+    console.log(request);
+    return request;
+  }
+
+  public hasFilter(filterName: string, value: string): boolean {
+    for (const filter of this[filterName]) {
+      if (filter === value) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private getFiltersName(): string[] {
+    let properties: string[] = [];
+
+    for (const prop in this) {
+      properties.push(prop);
+    }
+    return properties;
+  }
+
+  private constructFilters(): string {
+    let first: boolean = true;
+    let filters: string = '';
+
+    for (let filter of this.getFiltersName()) {
+      if (first && this[filter].length > 0) {
+        filters += this.constructFilter(filter);
+        first = false;
+      }
+      else if (this[filter].length > 0) {
+        filters += ' AND ' + this.constructFilter(filter);
+      }
+    }
+    return filters;
+  }
+
+  public add(filterName: string, value: string): void {
+    if (!this.hasFilter(filterName, value)) {
+      this[filterName].push(value);
+    }
+  }
+
+  public remove(filterName: string, value: string): void {
+    for (let i = 0; i < this[filterName].length; i++) {
+      if (this[filterName][i] === value) {
+        this[filterName].splice(i, 1);
+      }
+    }
+  }
+
+  public constructFilter(filterName: string): string {
+    let first: boolean = true;
+    let filter: string = '';
+
+    for (const element of this[filterName]) {
+      if (first) {
+        filter += filterName + ':' + "\"" + element + "\"";
+        first = false;
+      }
+      else {
+        filter += ' OR ' + filterName + ':' + "\"" + element + "\"";
+      }
+    }
+    return filter;
+  }
 }
--- src/app/search-engine/search-engine.component.ts
+++ src/app/search-engine/search-engine.component.ts
@@ -1,7 +1,7 @@
 import { SearchQuery } from 'src/app/models/search-query';
 import { AlgoliaManagerService } from 'src/app/services/algolia-manager.service';
 import { Address } from 'src/app/models/address';
-import { GeoService, GeoSearch } from 'src/app/services/geo.service';
+import { GeoService } from 'src/app/services/geo.service';
 import { ActivatedRoute } from '@angular/router';
 import { AppComponent } from 'src/app/app.component';
 import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
@@ -18,6 +18,7 @@ export class SearchEngineComponent extends GenericComponent implements OnInit {
   @Input() placeholder: string = 'Rechercher un produit';
   @Input() filters: any;
   @ViewChild('state') state: ElementRef;
+  readonly toolsAndMachines = ['Outillage à main', 'Electrique / Filaire', 'Pneumatique / À air'];
   config: any;
   searchQuery: SearchQuery = new SearchQuery(this.geoService);
   searchQueryTmp: SearchQuery = new SearchQuery(this.geoService);
@@ -58,5 +59,25 @@ export class SearchEngineComponent extends GenericComponent implements OnInit {
         this.searchQuery.zipcode = address.zipcode;
       }
     }
+  }
+
+  public setFilter(filterName: string, value: string): void {
+    if (this.searchQuery.filters.hasFilter(filterName, value)) {
+      this.searchQuery.filters.remove(filterName, value);
+    }
+    else {
+      this.searchQuery.filters.add(filterName, value);
+    }
+    this.updateSearch();
+  }
+
+  public setFilters(filterName: string, values: string[]): void {
+    for (const value of values) {
+      this.setFilter(filterName, value);
+    }
+  }
+
+  public updateSearch(): void {
+    this.searchQuery.searchParams.filters = this.searchQuery.filters.getAll();
   }
 }
