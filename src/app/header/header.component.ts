76be629ab8c11f6f2e5f0a901a5ef0ffc602d993
Klaymant <c.gauthier94250@gmail.com>
2021-03-21 19:24:14 +0100

    Fix display of menu after leaving a modal without closing it

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
+import { Location } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { MenuService } from 'src/app/services/menu.service';
 
 @Component({
   selector: 'app-header',
@@ -9,4 +11,13 @@ export class HeaderComponent {
   @Input() toDisplay: boolean;
   @Input() toDisplayMenu: boolean;
   @Input() logged: boolean;
+
+  constructor(
+    private location: Location,
+    private menu: MenuService)
+  {
+    this.location.onUrlChange((val) => {
+      this.menu.showMenu();
+    });
+  }
 }
