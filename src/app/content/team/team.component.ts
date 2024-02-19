f941cea7b3703bdb6aef14b687f6c88c50ee93bf
klaymant <clement.gauthier@protonmail.com>
2024-02-19 11:32:52 +0100

    Use media base url from environment for team members' images

--- src/app/content/team/team.component.ts
+++ src/app/content/team/team.component.ts
@@ -11,13 +11,12 @@ export class TeamComponent implements OnInit {
   members: TeamMember[] = [];
   displaySecondPhoto: [boolean, boolean, boolean, boolean];
 
-  constructor(private meta: MetaDataService)
-  {
+  constructor(private meta: MetaDataService) {
     this.members.push(
       new TeamMember(
         "Christophe Vincent",
-        "https://media.outilly.com/team/christophe-vincent-cso-cofondateur-outilly-1.jpg",
-        "https://media.outilly.com/team/christophe-vincent-cso-cofondateur-outilly-2.jpg",
+        "christophe-vincent-cso-cofondateur-outilly-1.jpg",
+        "christophe-vincent-cso-cofondateur-outilly-2.jpg",
         "Chief Sales Officer",
         "Le bricolo de l'équipe, mais surtout chineur de bonnes affaires et collectionneur d'outils ! J'ai fait mes armes derrière les fourneaux avant de me reconvertir dans l'industrie de BTP en tant que responsable commercial au sein du groupe Soprema.",
         "Christophe Vincent CSO et cofondateur d'Outilly",
@@ -26,8 +25,8 @@ export class TeamComponent implements OnInit {
     this.members.push(
       new TeamMember(
         "Yannick Piault",
-        "https://media.outilly.com/team/yannick-piault-ceo-cofondateur-outilly-2.jpg",
-        "https://media.outilly.com/team/yannick-piault-ceo-cofondateur-outilly-1.jpg",
+        "yannick-piault-ceo-cofondateur-outilly-2.jpg",
+        "yannick-piault-ceo-cofondateur-outilly-1.jpg",
         "Chief Executive Officer",
         "J'ai regardé 24 fois les saisons de H, je porte une barbe noire et j'ai des cheveux longs pour éviter les aller-retour chez le coiffeur, selon moi le fromage est un mets constitué de lait frelaté et je voue presque un culte à la langue française. Sinon je fais du marketing et j'aime innover. Voilà.",
         "Yannick Piault CEO et cofondateur d'Outilly",
@@ -36,8 +35,8 @@ export class TeamComponent implements OnInit {
     this.members.push(
       new TeamMember(
         "Samyr Rakoto",
-        "https://media.outilly.com/team/samyr-rakoto-cto-cofondateur-outilly-2.jpg",
-        "https://media.outilly.com/team/samyr-rakoto-cto-cofondateur-outilly-1.jpg",
+        "samyr-rakoto-cto-cofondateur-outilly-2.jpg",
+        "samyr-rakoto-cto-cofondateur-outilly-1.jpg",
         "Chief Technical Officer",
         "La légende raconte que je ne dors pas et que j'aurais inventé une formule chimique qui transforme mes heures de sommeil, ma passion pour la tech et le \"bien faire les choses\" en électricité pour faire tourner le serveur de production. Si vous lisez ceci, c'est que la formule fonctionne plutôt bien.",
         "Samyr Rakoto CTO et cofondateur d'Outilly",
@@ -46,14 +45,14 @@ export class TeamComponent implements OnInit {
     this.members.push(
       new TeamMember(
         "Clément Gauthier",
-        "https://media.outilly.com/team/clement-gauthier-lead-front-dev-cofondateur-outilly-1.jpg",
-        "https://media.outilly.com/team/clement-gauthier-lead-front-dev-cofondateur-outilly-2.jpg",
+        "clement-gauthier-lead-front-dev-cofondateur-outilly-1.jpg",
+        "clement-gauthier-lead-front-dev-cofondateur-outilly-2.jpg",
         "Lead Front Dev",
         "Passionné de musculation, de création musicale, de rédaction et de macramé (un intrus s'est glissé dans la liste), je passe beaucoup trop de temps à me poser des questions auxquelles personne n'a jamais pensé et à faire des jeux de mots beaucoup trop honteux. Et parfois il m'arrive de coder aussi.",
         "Clément Gauthier Lead Front Dev et cofondateur d'Outilly",
         "clément-gauthier-483239101"
       ));
-      this.displaySecondPhoto = [false, false, false, false];
+    this.displaySecondPhoto = [false, false, false, false];
    }
 
   ngOnInit(): void {
--- src/app/models/team-member.ts
+++ src/app/models/team-member.ts
@@ -1,4 +1,6 @@
-export class TeamMember{
+import { environment } from 'src/environments/environment';
+
+export class TeamMember {
   name: string;
   imgPath: string;
   imgPath2: string;
@@ -7,14 +9,17 @@ export class TeamMember{
   imgAlt: string;
   linkedin: string;
 
-  constructor(name: string, imgPath:string, imgPath2: string, title:string, description: string, imgAlt: string, linkedin: string)
-  {
+  constructor(name: string, imgPath:string, imgPath2: string, title:string, description: string, imgAlt: string, linkedin: string) {
     this.name = name;
-    this.imgPath = imgPath;
-    this.imgPath2 = imgPath2;
+    this.imgPath = this.getImgPath(imgPath);
+    this.imgPath2 = this.getImgPath(imgPath2);
     this.title = title;
     this.description = description;
     this.imgAlt = imgAlt;
     this.linkedin = linkedin;
+  }
+
+  private getImgPath(imgName: string): string {
+    return environment.mediaBaseUri + 'team/' + imgName;
   }
 }
